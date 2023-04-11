3b7e2da383cee45d51db2da68cbd15d6f0fa63aa
JDugganQuinnipiac <jduggan@quinnipiac.edu>
2023-04-11 11:52:45 -0400

    Starting commit

--- src/main.java
+++ src/main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class main {
     public static void main(String args[]){
-        //start project
+
+        System.out.println("\nThank you for using GymTracker!\nAre you a gym employee or member? (Type '1' for Employee | '2' for Member)");
+        Scanner reader = new Scanner (System.in);
+        int status = reader.nextInt();
+        if(status == 1){
+
+        }
+        else if (status == 2){
+
+        }
+
     }
 }
